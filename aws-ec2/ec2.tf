dc637d6e11ef1f6a2fbad2586f2957abba870d40
Esmeralda Montoya <esmeraldamontoya@MacBook-Air-de-Esmeralda.local>
2023-04-27 17:55:40 -0500

    ec2 en proceso

---
+++ aws-ec2/ec2.tf
@@ -0,0 +1,11 @@
+provaider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws-instance" "ec2" {
+    ami = "ami-0a695f0d95cefc163"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "terraform-ec2"
+    }
+  }
