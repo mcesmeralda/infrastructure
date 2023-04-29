834df164dbe6ec8be504ca1f99d7ddd217e1a8dc
Esmeralda Montoya <esmeraldamontoya@MacBook-Air-de-Esmeralda.local>
2023-04-28 21:52:30 -0500

    finish

--- aws-ec2/ec2.tf
+++ aws-ec2/ec2.tf
@@ -1,11 +1,20 @@
-provider "aws" {
-    region = "us-east-1"
-}
-
-resource "aws-instance" "ec2" {
-    ami = "ami-0a695f0d95cefc163"
-    instance_type = "t2.micro"
-    tags = {
-        Name = "terraform-ec2"
-    }
+resource "aws_instance" "instancia-dev" {
+  ami = "ami-0a695f0d95cefc163"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.sg-dev.id]
+  key_name = "lab_ec2"
+  tags = {
+    Name = "ec2-${local.env_project}"
+    Project = local.project
+    Environment = local.env_project
   }
+}
+resource "aws_eip" "lb" {
+  instance = aws_instance.instancia-dev.id
+  vpc      = true
+  tags = {
+    Name = "ec2-${local.env_project}"
+    Project = local.project
+    Environment = local.env_project
+  }
+}
